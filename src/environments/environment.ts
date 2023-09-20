88d9b812ba6c8667de63b70d798bd9fabd0b1bed
Stefan Irrgang <122272798+StefanIrrgang@users.noreply.github.com>
2023-09-20 14:04:05 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'sinotes-371d6',
+    appId: '1:266136521942:web:d6a23bf4e42162acaf4d77',
+    storageBucket: 'sinotes-371d6.appspot.com',
+    apiKey: 'AIzaSyCN7lrrLYCP4kNnd0aZboyCNJXrNa-iefc',
+    authDomain: 'sinotes-371d6.firebaseapp.com',
+    messagingSenderId: '266136521942',
+  },
+};
